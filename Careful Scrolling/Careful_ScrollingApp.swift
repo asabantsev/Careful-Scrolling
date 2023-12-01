69e5f7948b3c226ac3a4b1af8df97d62880945e7
asabantsev <asabantsev@gmail.com>
2023-12-01 18:52:21 +0300

    Initial Commit

---
+++ Careful Scrolling/Careful_ScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Careful_ScrollingApp.swift
+//  Careful Scrolling
+//
+//  Created by Aleksey Sabantsev on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Careful_ScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
